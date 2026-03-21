b37f49d7c83c7abfe5d62972848db7c96dd73fe3
NeoJhonn <jhonny.azevedo@gmail.com>
2026-03-21 00:40:10 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/br/com/jhonnyazevdo/timegrid_backend/TimegridBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.jhonnyazevdo.timegrid_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimegridBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimegridBackendApplication.class, args);
+	}
+
+}
