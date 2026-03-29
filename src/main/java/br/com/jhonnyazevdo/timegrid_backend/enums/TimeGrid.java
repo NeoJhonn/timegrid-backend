d08026970d90bc3b9bb90766e9646ab6e3f427d1
NeoJhonn <jhonny.azevedo@gmail.com>
2026-03-29 01:29:51 -0300

    feat: add TimeGrid and UserRole enums

---
+++ src/main/java/br/com/jhonnyazevdo/timegrid_backend/enums/TimeGrid.java
@@ -0,0 +1,34 @@
+package br.com.jhonnyazevdo.timegrid_backend.enums;
+
+public enum TimeGrid {
+
+    T0800,
+    T0830,
+    T0900,
+    T0930,
+    T1000,
+    T1030,
+    T1100,
+    T1130,
+    T1200,
+    T1230,
+    T1300,
+    T1330,
+    T1400,
+    T1430,
+    T1500,
+    T1530,
+    T1600,
+    T1630,
+    T1700,
+    T1730,
+    T1800,
+    T1830,
+    T1900,
+    T1930,
+    T2000,
+    T2030,
+    T2100,
+    T2130,
+    T2200
+}
